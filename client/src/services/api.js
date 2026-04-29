8a4a51037057a7cc4426e73b8f625fb44d84d186
sujal831 <sujalkumrawat831@gmail.com>
2026-04-29 14:36:19 +0530

    updated frontend for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+   baseUrl: "https://attendance-management-system-backend-m5rb.onrender.com/api",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token");
       if (token) {
